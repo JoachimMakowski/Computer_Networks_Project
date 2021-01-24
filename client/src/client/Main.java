8b431c0e6f590baf9b6197f37a41a585d9e6eb02
ColdBacon <bogumila.walkowiak@op.pl>
2021-01-24 02:18:56 +0100

    small changes to loading fxml files and logging off

--- client/src/client/Main.java
+++ client/src/client/Main.java
@@ -1,18 +1,25 @@
 package client;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /*      0 - login
+        00 - user already logged
+        01 - limit of active user on the server
+        02 - limit of users saved to the server
         1 - logout
         2 - join room
+        20 - successfully joined the room
+        21 - there is no room with that name
         3 - create new room
+        30 - room created successfully
+        31 - room with that name already exists
+        32 - out of space on the server (10 rooms)
         4 - message room
         5 - message face2face
 */
@@ -31,7 +38,6 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
-
         primaryStage.setOnCloseRequest(e ->
         {
             e.consume();
@@ -39,50 +45,14 @@ public class Main extends Application {
         });
     }
 
-
-    public static void login() throws IOException {
-        Parent menu = FXMLLoader.load(Main.class.getResource("menu.fxml"));
-        Stage menuStage = new Stage();
-        //menuStage = Main.parentWindow;
-        parentWindow.close();
-        parentWindow = menuStage;
-        menuStage.setTitle("Internet Relay Chat");
-        //menuStage.setMaximized(true);
-        menuStage.setResizable(false);
-        menuStage.setScene(new Scene(menu, 900, 700));
-        menuStage.show();
-
-        menuStage.setOnCloseRequest(e ->
-        {
-            e.consume();
-            closeProgram(menuStage);
-        });
-    }
-
-    public static void logout() throws IOException {
-        Parent menu = FXMLLoader.load(Main.class.getResource("login.fxml"));
-        Stage logoutStage = new Stage();
-        parentWindow.close();
-        parentWindow = logoutStage;
-        logoutStage.setTitle("Logout from IRC app");
-        logoutStage.setResizable(false);
-        logoutStage.setScene(new Scene(menu, 400,400));
-        logoutStage.show();
-
-        logoutStage.setOnCloseRequest(e ->
-        {
-            e.consume();
-            closeProgram(logoutStage);
-        });
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 
-    private static void closeProgram(Stage primaryStage) {
+    public static void closeProgram(Stage primaryStage) {
         Boolean answer = ConfirmBox.display("EXIT", "Sure you want to exit?", "YES", "NO");
         if (answer)
             primaryStage.close();
     }
+
 }
