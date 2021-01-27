876c27b667d5c4de7e36353cb29eecf57b874e8a
ColdBacon <bogumila.walkowiak@op.pl>
2021-01-28 00:24:16 +0100

    exceptions in connection with server

--- client/src/client/Main.java
+++ client/src/client/Main.java
@@ -9,30 +9,35 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /*      0 - login
-        00 - user logged successfully
-        01 - user already logged
-        02 - limit of active user on the server
-        03 - limit of users saved to the server
+            00 - user logged successfully
+            01 - user already logged
+            02 - limit of active user on the server
+            03 - limit of users saved to the server
         1 - logout
         2 - join room
-        20 - successfully joined the room
-        21 - there is no room with that name
+            20 - successfully joined the room
+            21 - there is no room with that name
         3 - create new room
-        30 - room created successfully
-        31 - room with that name already exists
-        32 - out of space on the server (10 rooms)
+            30 - room created successfully
+            31 - room with that name already exists
+            32 - out of space on the server (10 rooms)
         4 - message room
         5 - message face2face
         6 - change room (username, type of conversation from ('0' room, '1' chat), room name/username2 from,
             type of conversation to ('0' room, '1' chat), room name/username2 to)
+        7 - actual logged user
+            70\n%s\n - actually logged users %s = list of users
 */
 
 public class Main extends Application {
 
     private static Stage parentWindow;
+    private static ServerConnection serverConnection;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        serverConnection = new ServerConnection();
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         parentWindow = primaryStage;
         primaryStage.getIcons().add(new Image("file:icon.png"));
