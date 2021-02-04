d0f494261f3d8b5c2dcbc1a3cbf8e647e824517b
JoachimMakowski <joachim.makowski@student.put.poznan.pl>
2021-02-04 21:22:16 +0100

    small changes

--- server/user.c
+++ server/user.c
@@ -1,7 +1,8 @@
 #pragma once
+#define MAX_LENGHT_NAME 25
 
 typedef struct{//ewentualnie dodać kolor lub czy_zalogowany
-    char user[25];
+    char user[MAX_LENGHT_NAME];
     bool logged;
     int descriptor;
 }User;
